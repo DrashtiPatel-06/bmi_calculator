5f578b5de42a5ce0f12a3dda50128f2535e8213c
DrashtiPatel-06 <96313801+DrashtiPatel-06@users.noreply.github.com>
2022-08-02 14:22:41 +0530

    initial commit of full repository

---
+++ lib/compnents/bottom_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({required this.onTap, required this.buttonTitle});
+
+  final String buttonTitle;
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kbottomContainerColor,
+        margin: EdgeInsets.only(top: 10.0),
+        width: double.infinity,
+        height: kbottomContainerHeight,
+      ),
+    );
+  }
+}
